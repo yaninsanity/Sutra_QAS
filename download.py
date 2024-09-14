180281f1d423a65fc660d073061826264cc25ad6
zeren yan <38341107+yaninsanity@users.noreply.github.com>
2024-09-14 14:49:16 -0700

    add back missing nltk model and its requirement

--- download.py
+++ download.py
@@ -1,4 +1,9 @@
 import nltk
+import ssl
+
+# Bypass SSL verification
+ssl._create_default_https_context = ssl._create_unverified_context
+
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('punkt')
